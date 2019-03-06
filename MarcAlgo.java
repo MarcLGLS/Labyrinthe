f590d532e2ef98a41a61bceeb30d767773c4fdf7
MarcLGLS <47967041+MarcLGLS@users.noreply.github.com>
2019-03-06 17:16:13 +0100

    MarcAlgo (modif)

--- MarcAlgo.java
+++ MarcAlgo.java
@@ -26,12 +26,13 @@ public class MarcAlgo extends Generation{
   	// selection du case en bordure au hasard
 
     	int a = (int)(Math.random() * (hauteur+1));
+    	
   		int position[] = {0,a};
-  		int position1[] = {0,0};
+  		int position1[] = {1,a};
   		visite[0][a] = true;
   		boolean finie = false;
   	// tableau représentant l'état de la case départ
-  		boolean depart[] = {false,true,false,true};
+  		boolean depart[] = {false,true,true,true};
   		tableau[0][a].setMurs(depart);
 
 
@@ -39,32 +40,68 @@ public class MarcAlgo extends Generation{
 	// début remplissage de la grille
 		int i = 0;
 
-  		while( i< 25) {
-
+  		while( i< 100) {
+                                              
   			// choix de la case adjacente
-
+				int tour = 0;
   				do{
+					position1[0] = position[0];
+					position1[1] = position[1];
   					if((int)(2*Math.random())==0) {
   						position1[0]= position[0] +(int)(3*Math.random())-1;
+  						// position1[0] = position[0]+1;
   					} else {
+						// position1[1] = position[1]+1;
   						position1[1] = position[1] + (int)(3*Math.random())-1;
+  					
   					}
-  					System.out.println("MARC !! ");
-  				}while(position1[0]<0 || position1[0]>= largeur || position1[0] == position[0] ||position1[1]<0 || position1[1]>= hauteur || position1[1] == position[1] || visite[position1[0]][ position1[1]] == true );
-				
-				
-  				position[0] = position1[0];
-  				position[1] = position1[1];
-				
-  			// choix mur à détruire bout de code à compléter !!!!!!!!!!!!!!!!
-			maGrille.tableau[position[0]][position[1]].setMurs(0,false);
-			maGrille.tableau[position[0]][position[1]].setMurs(1,false);
-			maGrille.tableau[position[0]][position[1]].setMurs(2,false);
-			maGrille.tableau[position[0]][position[1]].setMurs(3,false);
-	
-			System.out.println("test"+i+" !! ");
+  					tour ++;
+  					
+  					if(tour >40) {
+						System.out.println("Je suis bloqué, sortez moi de cette boucle");
+						break;
+					}
+					
+					
+  				}while(position1[0]<0 || position1[0]>= largeur ||position1[1]<0 || position1[1]>= hauteur || visite[position1[0]][ position1[1]] == true);
 			
-
+			// destruction du mur choix. 
+			if( position[0] > position1[0]) {
+				maGrille.tableau[position[0]][position[1]].setMurs(0,false);
+				maGrille.tableau[position1[0]][position1[1]].setMurs(2,false);
+				System.out.println("Je casse le mur 0 et 2");
+			}
+			
+			if(position[0] < position1[0]) {
+				maGrille.tableau[position[0]][position[1]].setMurs(2,false);
+				maGrille.tableau[position1[0]][position1[1]].setMurs(0,false);
+				System.out.println("Je casse le mur 0 et 2");
+			}
+			
+			if( position[1] > position1[1]) {
+				maGrille.tableau[position1[0]][position1[1]].setMurs(3,false);
+				maGrille.tableau[position[0]][position[1]].setMurs(1,false);
+				System.out.println("Je casse le mur 1 et 3");
+			}
+			
+			if( position[1] < position1[1]) {
+				maGrille.tableau[position1[0]][position1[1]].setMurs(1,false);
+				maGrille.tableau[position[0]][position[1]].setMurs(3,false);
+				System.out.println("Je casse le mur 1 et 3");
+				
+			}
+				
+			
+				
+  				
+				
+  		
+	
+			System.out.println("test"+i+"/ x="+position1[0]+"  y="+position1[1]+"  ");
+			System.out.println("***********");
+			
+			position[0] = position1[0];
+  			position[1] = position1[1];
   			visite[position[0]][position[1]] = true;
 
 
