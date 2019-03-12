97aa90a4f3f77539157135f27a6b6cec04f73bb8
MarcLGLS <47967041+MarcLGLS@users.noreply.github.com>
2019-03-12 23:11:59 +0100

    Travail de mise en forme sur r&soudre = début résoudre à droite

---
+++ ResolutionDroite.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ResolutionDroite extends Resolution{
+	private LinkedList<Case> chemin; 
+	
+	public ResolutionDroite(Grille g){
+	this.maGrille = g;
+	this.chemin = new LinkedList<Case>();
+	}
+	
+	public void resoudre() {
+	  
+	  
+	}
+	
+}
--- resolution.java
+++ resolution.java
@@ -1,13 +1,11 @@
-public class resolution{
-public resolution{
-//on créé un "pion" virtuel qui va se déplacer sur le labyrinthe et qui va permettre de suivre l'avancement
-//on le met sur la case de depart
-int px=pxi;
-int py=pxi;
-//on créé un attribu "direction" au pion entre 0 et 3
-int dir=0;
-if(grille(px,py).2==false){}
-}}
+public abstract class Resolution{
+ protected Grille maGrille;
+
+ public abstract void resoudre();
+
+ 
+
+}
 
 
 
