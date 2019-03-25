b9f399f0e03f9ec31283ef8a635bc7535b184942
leprodelinfo <47974176+leprodelinfo@users.noreply.github.com>
2019-03-25 12:16:05 +0100

    ilot

--- Ilot.java
+++ Ilot.java
@@ -25,40 +25,43 @@ public class Ilot extends Generation implements Runnable{
     int i=0;
     
     while(i<(int)(c/2)){
-        i++;
+        
         //int dir = (int)(Math.random()*2);
         
-        for(int j=0; j<c-i; j++){
-                tableau[i][j].setMurs(2,false);
-                tableau[i][j].setMurs(0,false);
+            for(int j=i; j<c-i; j++){
+                tableau[j][i].setMurs(2,false);
+                //tableau[i][j].setMurs(0,false);
             }
-            for(int j=0; j<c-i; j++){
-                tableau[j][i].setMurs(3,false);
-                tableau[j][i].setMurs(1,false);
+            for(int j=i; j<c-i; j++){
+                tableau[i][j].setMurs(3,false);
+                //tableau[j][i].setMurs(1,false);
             }
-            for(int j=0; j<c-i;j++){
-                tableau[c-i][j].setMurs(2,false);
-                tableau[c-i][j].setMurs(0,false);
-            }
-            for(int j=0; j<c-i-1; j++){
-                tableau[j][c-i].setMurs(2,false);
-                tableau[j][c-i].setMurs(0,false);
+            for(int j=i; j<c-i;j++){
+                System.out.println("j="+j+" c="+c+" et i="+i);
+                tableau[j][c-i-1].setMurs(0,false);
                 
-                
+                //tableau[c-i][j].setMurs(0,false);
+            }
+            for(int j=i; j<c-i-1; j++){
+                tableau[c-i-1][j].setMurs(2,false);
+                //tableau[j][c-i].setMurs(0,false);
             }
         
         
-        if(i%2==0 /*dir==0*/){//ouverture en haut
-            tableau[i][(int)(Math.random()*c)].setMurs(3,false);
+        tableau[(int)(Math.random()*(c-i))][i].setMurs(1,false);
+        tableau[(int)(Math.random()*(c-i))][c-i-1].setMurs(3,false);
+        
+        /*if(i%2==0){//ouverture en haut
+            tableau[i][(int)(Math.random()*(c-i))].setMurs(1,false);
         }
         if(i%2==1){
-            tableau[c-i][(int)(Math.random()*c)].setMurs(1,false);
-        }
+            tableau[c-i][(int)(Math.random()*(c-i))].setMurs(3,false);
+        }*/
         
         
         
         
-        
+      i++;  
     }
     tableau[0][0].setEtat(Case.EtatCase.Depart);
     tableau[i][i].setEtat(Case.EtatCase.Arrivee);
