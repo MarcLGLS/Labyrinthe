ef94c264086a67b662c055df84170d9b7bf4b22c
leprodelinfo <47974176+leprodelinfo@users.noreply.github.com>
2019-04-04 21:55:52 +0200

    mise en forme ilot et unicité chemin

--- Ilot.java
+++ Ilot.java
@@ -22,48 +22,36 @@ public class Ilot extends Generation implements Runnable{
 
     Case[][] tableau = maGrille.getTableau();
     int c = (int)(Math.min(largeur, hauteur));
+    
     int i=0;
     
     while(i<(int)(c/2)){
         maGrille.attendreEtape();
         long h1 = System.nanoTime();
         
-        //int dir = (int)(Math.random()*2);
         
             for(int j=i; j<c-i-1; j++){
-                tableau[j][i].setMurs(2,false);
-                //tableau[i][j].setMurs(0,false);
+                tableau[j][i].setMurs(2,false);//on créé des couloirs horizontaux
             }
             for(int j=i; j<c-i-1; j++){
-                tableau[i][j].setMurs(3,false);
-                //tableau[j][i].setMurs(1,false);
+                tableau[i][j].setMurs(3,false);//on créé des couloirs verticaux
             }
             for(int j=i; j<c-i-1;j++){
-                System.out.println("j="+j+" c="+c+" et i="+i);
+                //System.out.println("j="+j+" c="+c+" et i="+i);
                 tableau[j+1][c-i-1].setMurs(0,false);
-                
-                //tableau[c-i][j].setMurs(0,false);
             }
             for(int j=i; j<c-i-1; j++){
                 tableau[c-i-1][j+1].setMurs(1,false);
-                //tableau[j][c-i].setMurs(0,false);
             }
         
         int r=(int)(Math.random()*(c-2*(i+1)))+1;
-        System.out.println("r = "+r);
+        //System.out.println("r = "+r);
         if(i%2==0){
-        tableau[r+i][i].setMurs(3,false);
+        tableau[r+i][i].setMurs(3,false);//on ouvre des portes entre les couloirs une fois en haut une fois en bas
         }
         if(i%2==1){
         tableau[r+i][c-i-1].setMurs(1,false);
         }
-        
-        /*if(i%2==0){//ouverture en haut
-            tableau[i][(int)(Math.random()*(c-i))].setMurs(1,false);
-        }
-        if(i%2==1){
-            tableau[c-i][(int)(Math.random()*(c-i))].setMurs(3,false);
-        }*/
         
         
         
--- uniciteChemin.java
+++ uniciteChemin.java
@@ -22,8 +22,6 @@ public class uniciteChemin extends Generation implements Runnable{
     int hauteur = maGrille.getHauteur();
 
     Case[][] tableau = maGrille.getTableau();
-    /*ArrayList<Arete> al = new ArrayList<Arete>();
-    ArrayList<Case> zone = new ArrayList<Case>();*/
     for(int i = 0; i<tableau.length; i++){
     for(int j = 0; j<tableau[i].length; j++){
         tableau[i][j].z = i*tableau.length+j;
@@ -38,15 +36,10 @@ public class uniciteChemin extends Generation implements Runnable{
     int xa=0; //coordonnee aléatoire dans le tableau
     int ya=0; //coordonnee aléatoire dans le tableau
 
-
-  //  Zone maZone = new Zone();//(lesAretes, lesSommets);
-    //ArrayList<Zone> lz = new ArrayList<Zone>(); // liste de zones
     boolean continuer = true;
-    int nbc = 0;
 
     if(largeur>1 && hauteur>1){//sécurité bug
-    while(continuer /*&& nbc < (hauteur)*(largeur)*/){
-        //while(Case c : maZone.lesAretes){//tant que toutes les cases du tableau ne sont pas dans la liste
+    while(continuer){
         maGrille.attendreEtape();
         long h1 = System.nanoTime();
 
@@ -54,72 +47,41 @@ public class uniciteChemin extends Generation implements Runnable{
 
             xa=(int)(Math.random()*(largeur));
             ya=(int)(Math.random()*(hauteur));
-            int f = tableau[xa][ya].z;
+            int f = tableau[xa][ya].z;//mémorise valeur case
 
 
-                    if((dir==0 && xa>0) || (dir==2 && xa<tableau[ya].length-1) || (dir==1 && ya>0) || (dir==3 && ya<tableau[xa].length-1)){//voisins existent
+                    if((dir==0 && xa>0) || (dir==2 && xa<tableau[ya].length-1) || (dir==1 && ya>0) || (dir==3 && ya<tableau[xa].length-1)){//voisins existent (notament pour les cases du bord)
                     if(tableau[xa][ya].getMurs()[dir]==true  && tableau[xa][ya].getVoisins()[dir].z != tableau[xa][ya].z){
-                        tableau[xa][ya].setMurs(dir,false);
-                      //  System.out.println("dir = "+dir+" xa = "+xa+" ya = "+ya);
-                        nbc=nbc+1;
-
-
-                        /*if(dir==0 && ya>0){
-                            tableau[xa][ya].z=tableau[xa][ya-1].z;
-                        }
-                        if(dir==2 && ya<tableau[xa].length-1){
-                            tableau[xa][ya].z=tableau[xa][ya+1].z;
-
-                        }
-                        if(dir==1 && xa>0){
-                            tableau[xa][ya].z=tableau[xa-1][ya].z;
-
-                        }
-                        if(dir==3 && xa<tableau.length-1){
-                            tableau[xa][ya].z=tableau[xa+1][ya].z;
-
-                        }*/
-
+                        tableau[xa][ya].setMurs(dir,false);//on casse le mur
 
 
                         if(dir==0 && xa>0){
-                            tableau[xa][ya].z=tableau[xa-1][ya].z;
-                            //al.add(new Arete(tableau[xa][ya], tableau[xa-1][ya]));
+                            tableau[xa][ya].z=tableau[xa-1][ya].z;//les cases sont dans la meme zone
+                            
                         }
                         if(dir==2 && xa<tableau[xa].length-1){
                             tableau[xa][ya].z=tableau[xa+1][ya].z;
-                            //al.add(new Arete(tableau[xa][ya], tableau[xa+1][ya]));
 
                         }
                         if(dir==1 && ya>0){
                             tableau[xa][ya].z=tableau[xa][ya-1].z;
-                            //al.add(new Arete(tableau[xa][ya], tableau[xa][ya-1]));
 
                         }
                         if(dir==3 && ya<tableau.length-1){
                             tableau[xa][ya].z=tableau[xa][ya+1].z;
-                            //al.add(new Arete(tableau[xa][ya], tableau[xa][ya+1]));
 
                         }
-
-                        //DEBUG : mettre les cases dans des listes
 
 
 
 
                     }
                     }
-                    /*for(Arete a : al){
-                        if(a.sommet1.z != a.sommet2.z){
-                            a.sommet1.z = a.sommet2.z;
-                        }
-                    }*/
-
 
                     for(int k =0; k<tableau.length; k++){
                     for(int l =0; l<tableau[k].length; l++){
                         if(tableau[k][l].z==f){
-                            tableau[k][l].z=tableau[xa][ya].z;
+                            tableau[k][l].z=tableau[xa][ya].z;//toutes les cases appartenant aux zones réunies sont dans la meme zone(avant il n'y avait que les deux cases adjacentes)
                         }
 
                     }}
@@ -128,24 +90,16 @@ public class uniciteChemin extends Generation implements Runnable{
 
 
 
-                    int m=0;
+                    int m=0;//sert à verifier si toutes les cases appartiennent à la meme zone (fin algo)
 
                     for(int k =0; k<tableau.length; k++){
                     for(int l =0; l<tableau[k].length; l++){
                         if(tableau[0][0].z != tableau[k][l].z){
                             m++;
                         }
-                        /*for(Arete a : al){
-                        if(a.sommet1.z != a.sommet2.z){
-                            a.sommet1.z = a.sommet2.z;
-                            //System.out.println("Sommet 1 = "+a.sommet1.z+" et sommet 2 = "+a.sommet2.z);
-                        }
-                    }*/
-
                     }
                     }
                     continuer = (m>0);
-                    //System.out.println("continuer = "+continuer+" car m = "+m);
 
 
         maGrille.ajouterTempsGene(System.nanoTime() - h1);
@@ -153,12 +107,7 @@ public class uniciteChemin extends Generation implements Runnable{
 
     }
     }
-    /*for(int k =0; k<tableau.length; k++){
-        for(int l =0; l<tableau[k].length; l++){
-            if(tableau[k][l].getMurs()[0]==true && tableau[k][l].getMurs()[1]==true && tableau[k][l].getMurs()[2]==true && tableau[k][l].getMurs()[3]==true){
-                tableau[k][l].setMurs((int)(Math.random()*4),false);
-            }
-        }}*/
+
 
 
     tableau[0][(int)(Math.random()*hauteur)].setEtat(Case.EtatCase.Depart);
