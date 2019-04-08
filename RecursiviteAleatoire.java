fb2798e0c4d40bf6ad2b5cf1b759d03074d80711
MarcLGLS <47967041+MarcLGLS@users.noreply.github.com>
2019-04-08 09:38:16 +0200

    Début commentaire recursivite aléatoire

--- RecursiviteAleatoire.java
+++ RecursiviteAleatoire.java
@@ -22,43 +22,56 @@ public class RecursiviteAleatoire extends Generation implements Runnable{
 		}
 
 	public void generer(){
-		long heure1 = System.nanoTime();  // H1 ..................................
-  		maGrille.remplir(true); // remplissage de la grille entierement par des murs
-    	Case[][] tableau = maGrille.getTableau(); // création d'un tableau de case associée à grille
+		long heure1 = System.nanoTime();  // Début du chronomètre.
+		
+		// Initialisation de la grille
+  		maGrille.remplir(true); // Remplissage de l'integralité de la grille par des murs.
+    	Case[][] tableau = maGrille.getTableau(); // Création d'un tableau d'objet case.
 
-		// selection de la position case de départ et création de celle-ci
+		// Selection aléatoire de la case départ du labyrinthe et création de celle-ci.
     	int a = (int)(Math.random() * (maGrille.getHauteur()));
   		int position[] = {0,a};
   		int position1[] = {0,a};
-  		visite[0][a] = true;
-  		boolean depart[] = {false,true,true,true};// tableau représentant l'état de la case départ
-		long heure2 = System.nanoTime();  // H2 ..................................
-		maGrille.ajouterTempsGene(heure2 - heure1);
+  		
+  		// Définition de la case départ
+  		visite[0][a] = true; // Définition de l'état visité de la case départ. 
+  		boolean depart[] = {false,true,true,true};// Tableau représentant l'état de la case départ.
+		
+		long heure2 = System.nanoTime();  // Fin du chronomètre 
+		maGrille.ajouterTempsGene(heure2 - heure1); // Enregistrement du temps d'éxécution.
 		 
-		maGrille.attendreEtape();
+		maGrille.attendreEtape(); // Methode synchrone utilisee pour attendre que la fenetre se mette a jour avant de modifier la grille.
 		
-		long heure3 = System.nanoTime();  // H2 
-  		tableau[0][a].setMurs(depart);// affectation des murs de la case départ
-		tableau[0][a].setEtat(Case.EtatCase.Depart);// affectation de l'état Depart de la case
-		long heure4 = System.nanoTime();  // H2 ..................................
-		maGrille.ajouterTempsGene(heure4 - heure3);
+		
+		long heure3 = System.nanoTime();  // Début du chronomètre.
+		
+		// Mise en place de la case départ.
+  		tableau[0][a].setMurs(depart);// Affectation des murs de la case départ
+		tableau[0][a].setEtat(Case.EtatCase.Depart);// Affectation de l'état Depart de la case.
+		
+		long heure4 = System.nanoTime();  // Fin du chronomètre
+		maGrille.ajouterTempsGene(heure4 - heure3); // Enregistrement du temps d'exécution.
 
 
-		maGrille.finEtape();
-		long heure5 = System.nanoTime();  // H2 
-		f(position1);// réalisation du premier chemin à partir de la case départ
-		long heure6 = System.nanoTime();  // H2 ..................................
-		maGrille.ajouterTempsGene(heure6 - heure5);
+		maGrille.finEtape(); // On indique qu'une etape vient de se finir, la fenetre va donc pourvoir se redessiner.
 		
-		// création de nouveaux chemins à partir de case déjà visitée tant que la grille n'est pas totalement visitée
+		// Réalisation du premier chemin
+		long heure5 = System.nanoTime();  // Début du chronomètre.
+		
+		chemin(position1);// Réalisation du premier chemin partant de la case départ.
+		
+		long heure6 = System.nanoTime();  // Fin du chronomètre.
+		maGrille.ajouterTempsGene(heure6 - heure5); // Enregistrement du temps d'exécution.
+		
+		// Creation de nouveaux chemins ayant comme point de départ une case visitée tant que le labyrinthe n'est pas intégralement complété.  		
 		do {
-			long heure7 = System.nanoTime();  // H2 ..................................
+			long heure7 = System.nanoTime(); // Début du chronomètre.
 			int x =0;
 			int y = 0;
 
 			
 			
-	
+			// Détermination aléatoire des coordonnées d'une case visitée.
 			do {
 				
 				x = (int)(Math.random()*(maGrille.getLargeur()));
@@ -66,36 +79,32 @@ public class RecursiviteAleatoire extends Generation implements Runnable{
 
 			} while(this.visite[x][y] == false);
 		
-				
-			
-
 			int position2[] = {x,y};
-			long heure8 = System.nanoTime();  // H2 ..................................
-			maGrille.ajouterTempsGene(heure4 - heure3);
 			
-			f(position2); // création d'un nouveau chemin à partir d'une case de coordonnées déterminées précédamment
+			long heure8 = System.nanoTime();  // Fin du chronomètre.
+			maGrille.ajouterTempsGene(heure8 - heure7); // Enregistrement du temps d'exécution. 
 			
-			
+			chemin(position2); // Création d'un nouveau chemin à partir de la case déterminée précédemment.
 
 
 		} while(finie()==false);
 
-		arrivee(); // création de la case arrivée du labyrinthe
+		arrivee(); // Création de la case arrivée du labyrinthe.
 		
 		maGrille.attendreEtape();
-		maGrille.geneEstFinie();
+		maGrille.geneEstFinie(); //On indique a la grille que la generation est finie, ainsi on peut lancer un algorithme de resolution.
 		maGrille.finEtape();
 
   }
 
-  //méthode vérifiant si l'intégralité du labyrinthe a été visité, renvoie true si labyrinthe complet et false si incomplet
+  //Méthode parcourant l'intégralité de la grille afin de savoir si toutes les cases ont été visité. Renvoie true si le labyrinthe finie et false dans le cas contraire.
 	public boolean finie() {
 		boolean finie = true;
   		for(int x=0; x<maGrille.getLargeur();x++) {
   			for(int y = 0; y<maGrille.getHauteur(); y++) {
 				maGrille.getTableau()[x][y].setEtat(Case.EtatCase.Normal);
 				if(this.visite[x][y] == false){
-  				finie = false;
+					finie = false;
   				}
 
   				}
@@ -104,10 +113,13 @@ public class RecursiviteAleatoire extends Generation implements Runnable{
   }
 
 
-  public void f(int a[]) {
-		long heure1 = System.nanoTime();  // H2 ..................................
+
+  public void chemin(int a[]) {
+		long heure1 = System.nanoTime();  // Début chronomètre.
+	    
 	    int largeur = maGrille.getLargeur();
 		int hauteur = maGrille.getHauteur();
+		
 	  	// début remplissage de la grille
 		int position[] = {a[0],a[1]};
 		int position1[] = {a[0],a[1]};
