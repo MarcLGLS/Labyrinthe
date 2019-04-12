5c948aa3dacd7f4b27bda75a9c1ac61a9ff29eb3
MarcLGLS <47967041+MarcLGLS@users.noreply.github.com>
2019-04-12 07:45:14 +0200

    Commentaire ResolutionDroite

--- ResolutionDroite.java
+++ ResolutionDroite.java
@@ -37,32 +37,33 @@ public class ResolutionDroite extends Resolution{
 
 
 		this.position = 2;
-		chemin(xd,yd);
-		long heure2 = System.nanoTime();
-		maGrille.ajouterTempsReso(heure2 - heure1); 
+		chemin(xd,yd); // Création du chemin ayant pour départ la case départ.
+		long heure2 = System.nanoTime(); // Fin chrono.
+		maGrille.ajouterTempsReso(heure2 - heure1); // Enregistrement du temps d'éxécution. 
 		maGrille.finReso();
 
 
 
 	}
-
+	// Méthode créant un chemin suivant le mur de droite.
 	public void chemin(int x, int y) {
 		
 		int x1 = x;
 		int y1 = y;
 		this.x=x;
 		this.y = y;
-		int i =0;
+		
+		
+		//On parcourt le labyrinthe tant que l'on ne rencontre pas la case arrivée.
 		do {
 			x1=this.x;
 			y1=this.y;
 		
 		maGrille.attendreEtape();
 		
-		// AVANCEMENT
+		// Choix de la case suivante, selon la configuration des murs
 		
 		// cas continuer tout droit
-
 		if(maGrille.getTableau()[x1][y1].getMurs(droite()) == true && maGrille.getTableau()[x1][y1].getMurs(gauche()) == true && maGrille.getTableau()[x1][y1].getMurs(this.position) == false ) {
 				avancement(x1,y1);
 				chemin.add(0,  maGrille.getTableau()[this.x][this.y]);
@@ -70,7 +71,6 @@ public class ResolutionDroite extends Resolution{
 		
 		// cas cul de sac faire demi tour
 		else if(maGrille.getTableau()[x1][y1].getMurs(droite()) == true && maGrille.getTableau()[x1][y1].getMurs(gauche()) == true && maGrille.getTableau()[x1][y1].getMurs(this.position) == true ) {
-				// faire demi tour
 				this.position = gauche();
 				this.position = gauche();
 				avancement(x1,y1);
@@ -107,7 +107,7 @@ public class ResolutionDroite extends Resolution{
 
 		}
 		
-				//cas intersection en L(tout droit et à droite)
+		//cas intersection en L(tout droit et à droite)
 		else if(maGrille.getTableau()[x1][y1].getMurs(droite()) == false && maGrille.getTableau()[x1][y1].getMurs(this.position) == false) {
 				this.position = droite();
 				avancement(x1,y1);
@@ -120,9 +120,10 @@ public class ResolutionDroite extends Resolution{
 		
 		
 		
-		maGrille.getTableau()[x1][y1].setEtat(Case.EtatCase.Chemin);
+		maGrille.getTableau()[x1][y1].setEtat(Case.EtatCase.Chemin); // Changement d'état des cases parcourues.
 		
 	
+		// Sélection des cases en doublons sauf les intersections afin de déterminer le chemin le plus rapide.
 		
 		int i1 = chemin.indexOf(maGrille.getTableau()[this.x][this.y]);
 		int i2 = chemin.lastIndexOf(maGrille.getTableau()[this.x][this.y]);
@@ -142,10 +143,10 @@ public class ResolutionDroite extends Resolution{
 		}
 		maGrille.finEtape();
 		
-		i++;
+		
 	}while(maGrille.getTableau()[this.x][this.y].getEtat() != Case.EtatCase.Arrivee);
 	
-	// suppresion des chemins !! 
+	// Disparition des cases en doublon sur l'affichage. 
 	for(Case c : cheminASupprimer) {
 		c.setEtat(Case.EtatCase.Normal);
 		
@@ -161,7 +162,7 @@ public class ResolutionDroite extends Resolution{
 /**
  * 
  */
- 
+	// Méthode faisant varier l'attribut position si on doit tourner à droite.
 	public int droite() {
 		
 		int droite = this.position + 1;
@@ -172,7 +173,8 @@ public class ResolutionDroite extends Resolution{
 		return droite;
 		
 	}
-
+	
+	// Méthode faisant varier l'attribut position si on doit tourner à gauche.
 	public int gauche() {
 		int gauche = this.position - 1;
 		if(gauche < 0)
@@ -183,9 +185,8 @@ public class ResolutionDroite extends Resolution{
 		
 	}
 	
-/**
- * Méthode modifiant les attributs x et y en fonction de la valeur de l'attribut position
- */
+
+	//Méthode modifiant les attributs x et y selon l'attribut position.
 	public void avancement(int x1, int y1) {
 		
 		if(this.position == 0)
